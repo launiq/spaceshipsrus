88c78ea54f20a18f53844b9218136bd2c5ca8f63
Joao Ribeiro <jprrib@gmail.com>
2024-02-15 23:15:41 +0000

    Implemented 'New' and 'Create' controller#actions and structured 'New' view

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,2 +1,22 @@
 class SpaceshipsController < ApplicationController
+
+  def new
+    @spaceship = Spaceship.new
+  end
+
+  def create
+    @spaceship = Spaceship.new(spaceship_params)
+    if @spaceship.save
+      redirect_to @spaceship
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def spaceship_params
+    params.require(:spaceship).permit(:name, :location, :rating, :cost)
+  end
+
 end
