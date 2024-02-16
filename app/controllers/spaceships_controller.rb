e44796335aaa6263ffa516bd0aa9961ab7574f13
Francesca Valsecchi <francesca_valsecchi@hotmail.com>
2024-02-16 20:28:58 +0100

    made corrections

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -24,6 +24,6 @@ class SpaceshipsController < ApplicationController
   private
 
   def spaceship_params
-    params.require(:spaceship).permit(:name, :location, :rating, :cost)
+    params.require(:spaceship).permit(:name, :location, :cost)
   end
 end
