32bf8568e9d3ff95030413209c62a13306c92089
GianClaudio Scarafini <g.claudio.scarafini@gmail.com>
2024-02-17 16:05:59 +0000

    add faker, edit the form, add the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,37 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 #hello world
+# Seeds with faker gem
+
+# create 10
+## require faker
+require 'faker'
+
+##SUDO
+# create 10 users
+# create 10 spaceships
+# for each user, create 1 spaceship
+
+
+p "creating 10 users"
+20.times do
+  user = User.new(
+    first_name:     Faker::Name.first_name,
+    last_name:      Faker::Name.last_name,
+    phone_number:   Faker::PhoneNumber.cell_phone,
+    email:          Faker::Internet.email
+  )
+  user.save!
+  p "the user#{user.first_name} has just been created"
+end
+
+for user in User.all
+  spaceship = Spaceship.new(
+    location:       Faker::Space.star_cluster,
+    name:           Faker::Space.launch_vehicle,
+    cost:           rand(1000..10000),
+    user: user
+  )
+  spaceship.save!
+  p "the spaceship#{spaceship.name} of the cost of #{spaceship.cost}has just been created for the user#{user.first_name}"
+end
