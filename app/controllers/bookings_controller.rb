0423e1282e26bab03423839c380186874e4b303f
Joao Ribeiro <jprrib@gmail.com>
2024-02-23 16:43:41 +0000

    start_date and end_date validations

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,7 +12,7 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to spaceship_path(@booking.spaceship)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
