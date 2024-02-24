d09ad29697e01fc83a54078367fac6ee7fbc4f8a
LauraNicolas <laura.cecilia.nicolas@gmail.com>
2024-02-24 17:09:44 +0100

    worked on the front end of the index-page, missing search bar, navbar and original seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_230038) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_23_182424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_230038) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "pending"
     t.date "start_date"
     t.date "end_date"
     t.index ["spaceship_id"], name: "index_bookings_on_spaceship_id"
